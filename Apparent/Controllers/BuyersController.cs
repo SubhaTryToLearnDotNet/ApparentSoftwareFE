ad2ed07cf48ddbff736e5a0f4eea5eee200373d6
Shubham Jagtap <116169685+Shubhamj2111@users.noreply.github.com>
2024-08-31 01:01:42 +0530

    buyer page changes

--- Apparent/Controllers/BuyersController.cs
+++ Apparent/Controllers/BuyersController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Apparent.DBContext;
+using Apparent.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,40 @@ namespace Apparent.Controllers
         // GET: Buyers
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                ProductDBContext product = new ProductDBContext();
+                ProductMaster model = new ProductMaster();
+                var dataSet = product.ProductList();
+                if (dataSet != null)
+                {
+                    var dt1 = dataSet.Tables[0];
+
+                    List<string> splitValuesList = new List<string>();
+
+                    for (int i = 0; dt1.Rows.Count > i; i++)
+                    {
+                        string columnValue = dt1.Rows[i]["category_name"].ToString();
+
+
+
+                        splitValuesList.Add(columnValue);
+                    }
+
+                    model.Getcategory = splitValuesList;
+                    model.DtProductList = dataSet.Tables[1];
+                    return View(model);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+
         }
     }
 }
